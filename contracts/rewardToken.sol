9ff1139b19d3a18d34f78ddb8f34866d035c3fdd
idogwu chinonso <idogwuchinonso1@gmail.com>
2023-02-16 07:17:24 +0100

    minor changes

---
+++ contracts/rewardToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakingToken is ERC20 {
+
+
+constructor( string memory _name, string memory _symbol, uint _ammount, address stakingContract) ERC20(_name, _symbol)  {
+_mint(stakingContract, _ammount );
+}
+
+}
